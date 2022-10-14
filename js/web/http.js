ef733b9d4d555952bf50a87f38f479386cc63dad
Antonio Alberto <antonioalbertodsb@gmail.com>
2022-10-13 23:19:17 -0300

    :sparkles: feat: Botão para atualizar conselho

--- js/web/http.js
+++ js/web/http.js
@@ -7,9 +7,13 @@ fetch(url)
   })
   .then((corpo) => {
     console.log(corpo.slip);
-    document.getElementById("id-message").innerHTML = 'ADVICE #';
+    document.getElementById("id-message").innerHTML = "ADVICE #";
     document.getElementById("id-message").innerHTML += corpo.slip.id;
     document.getElementById("text-message").innerHTML += '"';
     document.getElementById("text-message").innerHTML += corpo.slip.advice;
     document.getElementById("text-message").innerHTML += '"';
   });
+
+var btn = document.querySelector(".btn-reload");
+
+btn.addEventListener("click", () => location.reload());
